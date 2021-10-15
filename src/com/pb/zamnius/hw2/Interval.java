d6f3818da37a5d8617a870ce8349804bf1b2211d
Sergej Zamnius <sergej.zamnius@privatbank.ua>
2021-10-15 17:07:14 +0300

    init commit

--- src/com/pb/zamnius/hw2/Interval.java
+++ src/com/pb/zamnius/hw2/Interval.java
@@ -1,4 +1,21 @@
 package com.pb.zamnius.hw2;
+import java.util.Scanner;
 
 public class Interval {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x;
+        System.out.println("Уведіть ціле число: ");
+        x = scan.nextInt();
+        if (x>=0 && x<=14)
+            System.out.print("Число в інтервалі [0-14]");
+        else if (x>=15 && x<=35)
+            System.out.print("Число в інтервалі [15-35]");
+        else if (x>=36 && x<=50)
+            System.out.print("Число в інтервалі [36-50]");
+        else if (x>=51 && x<=100)
+            System.out.print("Число в інтервалі [51-100]");
+        else
+            System.out.print("Число не входить в жодний з інтервалів");
+    }
 }
